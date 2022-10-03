c7fdc6ba078e8cbfecc848e55105675baeda530c
Jared Slawski <jaredslawski@gmail.com>
2022-10-02 22:38:41 -0600

    Poses made and some minor gameplay startup stuff

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -9,6 +9,16 @@ public class GameManager : MonoBehaviour
 
     public int levelScore = 0;
 
+    [SerializeField]
+    private Transform[] poseZones;
+
+    private PoseData[] allPoses;
+
+    [SerializeField]
+    private PhotoTaker photoTaker;
+
+    public bool debug = false;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -16,6 +26,37 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
+
+        this.allPoses = Resources.LoadAll<PoseData>("Poses");
+
+        if (debug == false)
+        {
+            this.StartupGameSequence();
+        }
+    }
+
+    private void StartupGameSequence()
+    {
+        this.SetupNextPose();
+        this.photoTaker.StartTimer();
+    }
+
+    private void WrapUpGameSequence() { }
+
+    private void SetupNextPose()
+    {
+        levelScore = 0;
+
+        PoseData currentPose = this.allPoses[UnityEngine.Random.Range(0, this.allPoses.Length)];
+
+        Debug.LogError("Current Pose: " + currentPose.name);
+
+        this.poseZones[0].position = currentPose.headZonePosition;
+        this.poseZones[1].position = currentPose.torsoZonePosition;
+        this.poseZones[2].position = currentPose.rightArmZonePosition;
+        this.poseZones[3].position = currentPose.rightHandZonePosition;
+        this.poseZones[4].position = currentPose.leftArmZonePosition;
+        this.poseZones[5].position = currentPose.leftHandZonePosition;
     }
 
     // Update is called once per frame
--- Assets/_Scripts/GoalPoseMaker.cs
+++ Assets/_Scripts/GoalPoseMaker.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoalPose : MonoBehaviour
+public class GoalPoseMaker : MonoBehaviour
 {
     public PoseData poseData;
 
     private List<Transform> poseZones;
+
+    [SerializeField]
+    private List<Rigidbody> ragdollRbs;
+
+    public bool makePose = false;
 
     private void Awake()
     {
@@ -24,7 +29,17 @@ public class GoalPose : MonoBehaviour
         this.poseZones[5].position = this.poseData.leftHandZonePosition;
     }
 
-    private void Update()
+    private void MovePoseZones()
+    {
+        this.poseZones[0].position = this.ragdollRbs[0].position;
+        this.poseZones[1].position = this.ragdollRbs[1].position;
+        this.poseZones[2].position = this.ragdollRbs[2].position;
+        this.poseZones[3].position = this.ragdollRbs[3].position;
+        this.poseZones[4].position = this.ragdollRbs[4].position;
+        this.poseZones[5].position = this.ragdollRbs[5].position;
+    }
+
+    private void SavePoseZones()
     {
         this.poseData.headZonePosition = this.poseZones[0].position;
         this.poseData.torsoZonePosition = this.poseZones[1].position;
@@ -32,5 +47,16 @@ public class GoalPose : MonoBehaviour
         this.poseData.rightHandZonePosition = this.poseZones[3].position;
         this.poseData.leftArmZonePosition = this.poseZones[4].position;
         this.poseData.leftHandZonePosition = this.poseZones[5].position;
+    }
+
+    private void Update()
+    {
+        if (this.makePose == true)
+        {
+            this.MovePoseZones();
+            this.SavePoseZones();
+
+            this.makePose = false;
+        }
     }
 }
